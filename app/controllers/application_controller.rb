a0e20ec0c17c3467b601bd7681e6ec2a02aaa17c
Chris <arnold.christoph@gmx.net>
2016-04-13 11:06:20 +0200

    Update before deadline Medienprojekt-Dokumentaion - added comments in code - deleted unnecessary code from development - slimmed down code - added "no results"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,172 +4,27 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
  
   def index
-    @time = Time.now
 
+    # open a connection to the fuseki server
     sparql = SPARQL::Client.new("http://localhost:3030/dataset/")
     
-    ## SPARQL Query 
-    #SELECT ?a
-    #WHERE {
-    #  ?a ?b <http://www.semanticweb.org/libry/ontologies/2015/11/untitled-ontology-8#scenarios>
-    #}
-    #LIMIT 10
-    
-    ## Ruby Test-Query
-    #query = sparql.select(:a).where([:a, :b, RDF::URI('http://www.semanticweb.org/libry/ontologies/2015/11/untitled-ontology-8#scenarios')]).limit(10)
-   
-    #query = sparql.select.prefix(rdf: RDF::URI("http://www.w3.org/1999/02/22-rdf-syntax-ns#")).
-    #                      prefix(rdfs: RDF::URI("http://www.w3.org/2000/01/rdf-schema#")).
-    #                      prefix(owl: RDF::URI("http://www.w3.org/2002/07/owl#")).
-    #                      prefix(xsd: RDF::URI("http://www.w3.org/2001/XMLSchema#")).
-    #                      prefix(md: RDF::URI("http://www.semanticweb.org/simon/ontologies/2016/1/medienprojekt#")).
-    #                      where([
-    #                                  :name, RDF::URI("http://www.semanticweb.org/simon/ontologies/2016/1/medienprojekt#usableIn"), RDF::URI("http://www.semanticweb.org/simon/ontologies/2016/1/medienprojekt#spanish")
-                                      #:name md:promotes md:selfOrganized,
-                                      #:name md:requires md:smallgroup
-    #                                ]).limit(10)
-
-    ## !! Array ???      
-
-    data = Array.new
-    
-    hash = {:predicate => "usableIn", :object => "mathematics"}
-    data.push(hash)
-    hash = {:predicate => "promotes", :object => "selfDirected"}
-    data.push(hash)
-    hash = {:predicate => "includes", :object => "remember"}
-    data.push(hash)
-    hash = {:predicate => "mediates", :object => "factualKnowledge"}
-    data.push(hash)
-    hash = {:predicate => "requires", :object => "individualWork"}
-    data.push(hash)
-    
-    query = name_query_generator(data)
-       
-    #puts query
-    
-    result = sparql.query(query)
-
-    @web_results = Array.new
-    
-    result.each_solution do |solution|
-      @web_results << solution.inspect
-    end
-    
-    data = Array.new
-    
-    hash = {:object => "educationalSubjects"}
-    data.push(hash)
-    
-    query = object_query_generator(data)
-    
-    result = sparql.query(query)
-    
-    @educationalSubjects = Array.new
-    
-    result.each_solution do |solution|
-      @educationalSubjects << solution.inspect
-    end
-
-    data = Array.new
-    
-    hash = {:object => "arrangement"}
-    data.push(hash)
-    
-    query = object_query_generator(data)
-    
-    result = sparql.query(query)
-    
-    @arrangement = Array.new
-    
-    result.each_solution do |solution|
-      @arrangement << solution.inspect
-    end
-
-    data = Array.new
-    
-    hash = {:object => "cognitiveProcess"}
-    data.push(hash)
-    
-    query = object_query_generator(data)
-    
-    result = sparql.query(query)
-    
-    @cognitiveProcess = Array.new
-    
-    result.each_solution do |solution|
-      @cognitiveProcess << solution.inspect
-    end
-
-    data = Array.new
-    
-    hash = {:object => "learningActivity"}
-    data.push(hash)
-    
-    query = object_query_generator(data)
-    
-    result = sparql.query(query)
-    
-    @learningActivity = Array.new
-    
-    result.each_solution do |solution|
-      @learningActivity << solution.inspect
-    end
-
-    data = Array.new
-    
-    hash = {:predicate => "rdf:type", :object => "selfDeterminationDegree"}
-    data.push(hash)
-    
-    query = object_query_generator(data)
-    
-    result = sparql.query(query)
-    
-    @selfDeterminationDegree = Array.new
-    
-    result.each_solution do |solution|
-      @selfDeterminationDegree << solution.inspect
-    end
-
-    data = Array.new
-    
-    hash = {:object => "competence"}
-    data.push(hash)
-    
-    query = object_query_generator(data)
-    
-    result = sparql.query(query)
-    
-    @competence = Array.new
-    
-    result.each_solution do |solution|
-      @competence << solution.inspect
-    end
-
-    data = Array.new
-    
-    hash = {:object => "knowledge"}
-    data.push(hash)
-    
-    query = object_query_generator(data)
-    
-    result = sparql.query(query)
-    
-    @knowledge = Array.new
-    
-    result.each_solution do |solution|
-      @knowledge << solution.inspect
-    end
-   
-    @time = Time.now
-
+    # generate the dropdown menus for the search options
+    @educationalSubjects = get_object_results({:object => "educationalSubjects"}, sparql)
+    @arrangement = get_object_results({:object => "arrangement"}, sparql)
+    @cognitiveProcess = get_object_results({:object => "cognitiveProcess"}, sparql)
+    @learningActivity = get_object_results({:object => "learningActivity"}, sparql)
+    @selfDeterminationDegree = get_object_results({:predicate => "rdf:type", :object => "selfDeterminationDegree"}, sparql)
+    @competence = get_object_results({:object => "competence"}, sparql)
+    @knowledge = get_object_results({:object => "knowledge"}, sparql)
   end
   
-      ## Suchanfrage
+  ## Suchanfrage
   
   def search
+    # open a connection to the fuseki server
     sparql = SPARQL::Client.new("http://localhost:3030/dataset/")    
 
+    # query header
     query = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
              PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
              PREFIX owl: <http://www.w3.org/2002/07/owl#>
@@ -179,6 +34,7 @@ class ApplicationController < ActionController::Base
              SELECT *
              {"
 
+    # disassemble the parameters of the function
     for searchTerm in params[:id].split(',')
       name = searchTerm.split('_')[0]
       if name == 'educationalSubjects'
@@ -196,21 +52,31 @@ class ApplicationController < ActionController::Base
       if name == 'arrangement'
         predicate = "requires"
       end
+      # add the parameters to the query
       query = query << "?name :" << predicate << " :" << searchTerm.split('_')[1] << " .\n"
     end
 
+    # query footer
     query  = query << "      }\nLIMIT 400"
     
+    # send the query to the sparql server and get back the results    
     result = sparql.query(query)
     
+    # create correct html entries to pass to the website
     sresult = ""
        
     result.each_solution do |solution|
       sresult << "<li><a href=\"" << solution.inspect[/medienprojekt#.*>/][14..-5].downcase << "\" data-toggle=\"modal\" data-target=\"#" << solution.inspect[/medienprojekt#.*>/][14..-5].downcase << "\" style=\"text-decoration: underline\">" << solution.inspect[/medienprojekt#.*>/][14..-5].capitalize << "</a></li><br>"
     end
+    
+    # check if results were found, if not display a no results message on the webpage
+    if sresult == ""
+      sresult = "No results."
+    end
  
     @sresult = { "content" => sresult}
  
+    # render the results on the webpage
     respond_to do |format|
       format.html
       format.json { render json: @sresult }
@@ -218,25 +84,9 @@ class ApplicationController < ActionController::Base
     
   end
 
-  def name_query_generator(data)
-    query = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-             PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-             PREFIX owl: <http://www.w3.org/2002/07/owl#>
-             PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-             PREFIX :  <http://www.semanticweb.org/simon/ontologies/2016/1/medienprojekt#>
-            
-             SELECT *
-             {"
-               
-    data.each {
-                |hash| query = query << "?name :" << hash[:predicate] << " :" << hash[:object] << " .\n"
-              }
-
-    query = query << "      }\nLIMIT 400"
-    return query
-  end
-
+  ## Generates query strings for objects
   def object_query_generator(data)
+    # query header
     query = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
              PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
              PREFIX owl: <http://www.w3.org/2002/07/owl#>
@@ -245,19 +95,45 @@ class ApplicationController < ActionController::Base
             
              SELECT *
              {"
-               
+    
+    # combine the parameters with the query
     data.each {
-                |hash| if hash.has_key?(:predicate) 
+                |hash| if hash.has_key?(:predicate)
+                         # its possible to have predicate and object parameters 
                          query = query << "?name " << hash[:predicate] <<  " :" << hash[:object] << " .\n"
                        else
+                         # or just an object parameter
                          query = query << "?name ?predicate :" << hash[:object] << " .\n"
                        end
             }
 
+    # query footer
     query = query << "      }\nLIMIT 400"
-    puts query
+    # return the resulting query
     return query
   end
- 
- end
+  
+  # get back the options for object parameters  
+  def get_object_results(hash, sparql)
 
+    # create a new Array to parameterize the query generation
+    data = Array.new
+    
+    data.push(hash)
+    
+    # generate the query
+    query = object_query_generator(data)
+    
+    # send the query to the sparql database and get back the results
+    result = sparql.query(query)
+    
+    # create a data structure to pass data to the webpage and safe the results in it
+    return_value = Array.new
+    
+    result.each_solution do |solution|
+      return_value << solution.inspect
+    end
+    
+    return return_value
+  end
+end
